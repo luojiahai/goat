4bfeee3739d1dc7876ea88739a76f7908ff8282c
ICEFIR <icefir1996@gmail.com>
2019-04-01 03:06:55 +1100

    Partially working

---
+++ src/GoatAST.hs
@@ -0,0 +1,68 @@
+module GoatAST where
+-- AST Part
+
+type Ident = String
+
+data BaseType 
+  = BoolType | IntType | FloatType
+    deriving (Show, Eq)
+
+data Lvalue 
+  = LId Ident
+    deriving (Show, Eq)
+
+data BBinOp
+  = And | Or
+    deriving (Eq,Show)
+  
+-- data BExpr 
+--   = BoolConst Bool 
+--   | Not BExpr
+--   | BBinary BBinOP BExpr BExpr
+--   | RBinary RBinOP AExpr AExpr
+--     deriving(Show)
+data Expr
+  = BoolConst Bool
+  | IntConst Int
+  | StrConst String
+  | Num Float
+  | Id Ident
+  | Add Expr Expr
+  | Mul Expr Expr
+  | UnaryMinus Expr
+    deriving (Show, Eq)
+    
+
+-- data RBinOP 
+--   = Equ
+--   | Greater 
+--   | GreaterEqu
+--   | Less
+--   | LessEqu
+--   | NotEqu
+--     deriving (Show)
+
+-- data ABinOp 
+--   = Add
+--   | Subtract
+--   | Multiply
+--   | Divide
+--     deriving (Show)
+
+data Decl 
+  = Decl Ident BaseType
+    deriving (Show, Eq)
+
+data Stmt 
+  = Assign Lvalue Expr
+  | Read Lvalue
+  | Write Expr
+  | Call Ident [Expr]
+  | If Expr [Stmt]
+  | IfElse Expr [Stmt] [Stmt]
+  | While Expr [Stmt]
+    deriving (Show, Eq)
+
+data GoatProgram
+  = Program [Decl] [Stmt]
+    deriving (Show, Eq)
