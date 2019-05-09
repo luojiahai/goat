e4930a0e0aa01632ebe4129a748ba49da1d3764f
JEFFREY <hoipck@gmail.com>
2019-05-09 10:48:49 +0000

    update

--- glaw/GoatAnalyze.hs
+++ glaw/GoatAnalyze.hs
@@ -14,51 +14,9 @@ module GoatAnalyze where
 
 import GoatParser
 import GoatAST
-import GoatSymTable as ST
-
-import qualified Data.Map
+import GoatSymTable
 
 
 analyze :: GoatProgram -> [SymTable]
-analyze (GoatProgram procs) = aProcs procs
+analyze goatProgram = symTable goatProgram
 
-aProcs :: [Procedure] -> [SymTable]
-aProcs [] = []
-aProcs [proc] = [aProc proc]
-aProcs (proc:procs) = aProc proc : (aProcs procs)
-
-aProc :: Procedure -> SymTable
-aProc (Procedure ident prmts decls stmts) = 
-  let hashMap = (aPrmts prmts . aDecls decls) emptyHashMap
-  in SymTable ident hashMap
-  where emptyHashMap = Data.Map.fromList([])
-
-aPrmts :: [Prmt] -> HashMap -> HashMap
-aPrmts [] hashMap = hashMap
-aPrmts [prmt] hashMap = aPrmt prmt hashMap
-aPrmts (prmt:prmts) hashMap = (aPrmt prmt . aPrmts prmts) hashMap
-
-aPrmt :: Prmt -> HashMap -> HashMap
-aPrmt (Prmt Val basetype name) hashMap = 
-  let (_, newHashMap) = ST.bind name (Symbol name attribute) hashMap
-  in newHashMap
-  where attribute = [Type basetype]
-aPrmt (Prmt Ref basetype name) hashMap = 
-  let (_, newHashMap) = ST.bind name (Symbol name attribute) hashMap
-  in newHashMap
-  where attribute = [Type basetype]
-
-aDecls :: [Decl] -> HashMap -> HashMap
-aDecls [] hashMap = hashMap
-aDecls [decl] hashMap = aDecl decl hashMap
-aDecls (decl:decls) hashMap = (aDecl decl . aDecls decls) hashMap
-
-aDecl :: Decl -> HashMap -> HashMap
-aDecl (Decl (Ident name) basetype) hashMap =
-  let (_, newHashMap) = ST.bind name (Symbol name attribute) hashMap
-  in newHashMap
-  where attribute = [Type basetype]
-aDecl (Decl (IdentWithShape name expr) basetype) hashMap =
-  let (_, newHashMap) = ST.bind name (Symbol name attribute) hashMap
-  in newHashMap
-  where attribute = [Type basetype]
--- glaw/GoatSymTable.hs
+++ glaw/GoatSymTable.hs
@@ -30,13 +30,57 @@ data Attribute = Slot Int | Type BaseType
 data Symbol = Symbol String [Attribute]
     deriving (Show, Eq)
 
-bind :: String -> Symbol -> HashMap -> ((), HashMap)
-bind key value hashMap =
+symTable :: GoatProgram -> [SymTable]
+symTable (GoatProgram procs) = stProcs procs
+
+stBind :: String -> Symbol -> HashMap -> ((), HashMap)
+stBind key value hashMap =
     let hashMap' = Data.Map.insert key value hashMap
     in ((), hashMap')
 
-lookup :: String -> HashMap -> (Symbol, HashMap)
-lookup key hashMap = 
+stLookup :: String -> HashMap -> (Symbol, HashMap)
+stLookup key hashMap = 
     case Data.Map.lookup key hashMap of
       Just value -> (value, hashMap)
       Nothing -> error $ "Undefined variable " ++ key
+
+stProcs :: [Procedure] -> [SymTable]
+stProcs [] = []
+stProcs [proc] = [stProc proc]
+stProcs (proc:procs) = stProc proc : (stProcs procs)
+
+stProc :: Procedure -> SymTable
+stProc (Procedure ident prmts decls stmts) = 
+  let hashMap = (stPrmts prmts . stDecls decls) emptyHashMap
+  in SymTable ident hashMap
+  where emptyHashMap = Data.Map.fromList([])
+
+stPrmts :: [Prmt] -> HashMap -> HashMap
+stPrmts [] hashMap = hashMap
+stPrmts [prmt] hashMap = stPrmt prmt hashMap
+stPrmts (prmt:prmts) hashMap = (stPrmt prmt . stPrmts prmts) hashMap
+
+stPrmt :: Prmt -> HashMap -> HashMap
+stPrmt (Prmt Val basetype name) hashMap = 
+  let (_, newHashMap) = stBind name (Symbol name attributes) hashMap
+  in newHashMap
+  where attributes = [Type basetype]
+stPrmt (Prmt Ref basetype name) hashMap = 
+  let (_, newHashMap) = stBind name (Symbol name attributes) hashMap
+  in newHashMap
+  where attributes = [Type basetype]
+
+stDecls :: [Decl] -> HashMap -> HashMap
+stDecls [] hashMap = hashMap
+stDecls [decl] hashMap = stDecl decl hashMap
+stDecls (decl:decls) hashMap = (stDecl decl . stDecls decls) hashMap
+
+stDecl :: Decl -> HashMap -> HashMap
+stDecl (Decl (Ident name) basetype) hashMap =
+  let (_, newHashMap) = stBind name (Symbol name attributes) hashMap
+  in newHashMap
+  where attributes = [Type basetype]
+stDecl (Decl (IdentWithShape name expr) basetype) hashMap =
+  let (_, newHashMap) = stBind name (Symbol name attributes) hashMap
+  in newHashMap
+  where attributes = [Type basetype]
